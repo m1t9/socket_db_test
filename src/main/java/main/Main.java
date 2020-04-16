5024cacd9e49dccead60990a1faf2bde9118e0c1
m1t9 <dpleshanov@gmail.com>
2020-04-16 04:29:36 +0300

    add log

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,14 +1,20 @@
 package main;
 
 import executor.DBService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.WebSocketChatServlet;
 
 public class Main {
+    static final Logger logger = LogManager.getLogger(Main.class.getName());
 
     public static void main(String[] args) throws Exception {
+
+//        System.setProperty("log4j.configurationFile", "cfg\\log4j2.xml");
+
         DBService dbService = new DBService();
         dbService.printConnectInfo();
 
@@ -19,6 +25,7 @@ public class Main {
         server.setHandler(context);
 
         server.start();
+        logger.info("Server started");
         System.out.println("Server started");
         server.join();
 
